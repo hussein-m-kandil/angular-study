9fadf840eafd74bef329c4063d6d67c624bdf736
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-03-25 11:20:01 +0200

    MDN Todo List: Initiate app component logic

--- projects/mdn-todo-list/src/app/app.component.ts
+++ projects/mdn-todo-list/src/app/app.component.ts
@@ -1,12 +1,38 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'mdn-todo-list';
+  title = 'Todo List';
+
+  filter: 'all' | 'done' | 'pending' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return [...this.allItems];
+    }
+    return [
+      ...this.allItems.filter((item) =>
+        this.filter === 'done' ? item.done : !item.done
+      ),
+    ];
+  }
+
+  addItem(description: string) {
+    if (!description) return;
+    this.allItems.unshift({ description, done: false });
+  }
 }
